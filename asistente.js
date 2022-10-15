a6f7a4d6df4c831df564e22d130110c2aba479da
Simon Castaño Rios <CASTANOSIMON2002@GMAIL.COM>
2022-10-15 13:36:49 -0500

    change interfaz and add commands

--- asistente.js
+++ asistente.js
@@ -3,6 +3,9 @@ Programa creado por youtube.com/ElTallerDeTD
 20/2/20
 https://github.com/imTDB
 */
+ 
+function alerta(){
+ 
 
 if (annyang) {
   //Variable para almacenar las voces de nuestro sistema.
@@ -11,8 +14,8 @@ if (annyang) {
   //Inicializamos utter.
   var utter = new SpeechSynthesisUtterance();
   utter.rate = 1;
-  utter.pitch = 0.5;
-  utter.lang = 'es-AR';
+  utter.pitch = 0.111111;
+  utter.lang = 'es';
 
   //Cargamos las voces que tenemos en nuestro sistema y las mostarmos en un arreglo por consola.
   window.speechSynthesis.onvoiceschanged = function () {
@@ -20,22 +23,51 @@ if (annyang) {
     console.log(voices);
   };
 
+  function RunFile() {
+    WshShell = new ActiveXObject("WScript.Shell");
+    WshShell.Run("c:/windows/system32/notepad.exe", 1, false);
+  }
+
   //Definimos los comandos a utilizar.
   var commands = {
     'hola maria': function () {
       utter.text = 'Hola usuario';
       //Setea la voz que queremos usar en base a nuestra lista.
-      utter.voice = voices[2];
+      utter.voice = voices[1];
       window.speechSynthesis.speak(utter);
     },
-    'como estas': function () {
+    'open youtube': function () {
+      utter.text = 'opening youtube mother fuckers';
+      window.open('https://www.youtube.com', '_blank');
+
+      //Setea la voz que queremos usar en base a nuestra lista.
+      utter.voice = voices[1];
+      window.speechSynthesis.speak(utter);
+    },
+    'open facebook': function () {
+      utter.text = 'opening facebook son of a bitch';
+      window.open('https://www.facebook.com', '_blank');
+
+      //Setea la voz que queremos usar en base a nuestra lista.
+      utter.voice = voices[1];
+      window.speechSynthesis.speak(utter);
+    },
+    'open app': function () {
+      utter.text = 'get out of here';
+      RunFile();
+
+      //Setea la voz que queremos usar en base a nuestra lista.
+      utter.voice = voices[1];
+      window.speechSynthesis.speak(utter);
+    },
+    'como estas': function () {     
       utter.text = 'Muy bien!';
-      utter.voice = voices[2];
+      utter.voice = voices[1];
       window.speechSynthesis.speak(utter);
     },
     hola: function () {
       utter.text = 'hola, cual es tu nombre?';
-      utter.voice = voices[2];
+      utter.voice = voices[1];
       window.speechSynthesis.speak(utter);
       //Guarda el nombre que le decimos por voz.
       annyang.addCallback('result', function (phrases) {
@@ -51,24 +83,29 @@ if (annyang) {
     //Array que devuelve aleatoriamente un elemento del array, en este caso un chiste.
     'cuentame un chiste': function () {
       chistes = [
-        'Por qué las focas del circo miran siempre hacia arriba?   Porque es donde están los focos.',
-        'Estas obsesionado con la comida!   No se a que te refieres croquetamente.',
-        'Por que estás hablando con esas zapatillas?   Porque pone "converse"',
-        'Buenos dias, me gustaria alquilar "Batman Forever".   No es posible, tiene que devolverla tomorrow.',
+        
+        'que es lo unico blanco que tiene un negro,... el dueño',
+        'que es lo unico blanco que tiene un negro,... el dueño',
+        'que es lo unico blanco que tiene un negro,... el dueño',
+        'que es lo unico blanco que tiene un negro,... el dueño',
+        'que es lo unico blanco que tiene un negro,... el dueño',
+        'que es lo unico blanco que tiene un negro,... el dueño',
+        
+        
       ];
       utter.text = chistes[Math.floor(Math.random() * chistes.length)];
-      utter.voice = voices[2];
+      utter.voice = voices[1];
       window.speechSynthesis.speak(utter);
     },
   };
 
   //Esto nos sirve para ver que escucha el programa en tiempo real.
-  /*
+  
     annyang.addCallback('result', function(phrases) {
       console.log("I think the user said: ", phrases[0]);
       console.log("But then again, it could be any of the following: ", phrases);
        });
-       */
+       
 
   //Sumamos todos los comandos a annyang.
   annyang.addCommands(commands);
@@ -76,3 +113,6 @@ if (annyang) {
   //Annyang comienza a escuchar.
   annyang.start();
 }
+
+}
+
