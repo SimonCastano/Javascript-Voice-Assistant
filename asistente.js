a9e1e30ca5e6e319b9bab5f6a0761628de5d0c27
SimonCastano <CASTANOSIMON2002@GMAIL.COM>
2022-10-20 12:55:02 -0500

    configure dark mode with scss

--- asistente.js
+++ asistente.js
@@ -5,28 +5,27 @@ https://github.com/imTDB
 */
 var startAssistant = document.getElementById('startAssistant');
 let isChecked;
-startAssistant.addEventListener('click', function()
-{
+startAssistant.addEventListener('click', function () {
   if (!annyang) {
-    return (alert("Lo siento, tu navegador no soporta el reconocimiento de voz :("));
+    return alert('Lo siento, tu navegador no soporta el reconocimiento de voz :(');
   }
   if (document.getElementById('startAssistant').checked);
   {
     // Variable para almacenar las voces de nuestro sistema.
     var voices;
-  
+
     // Inicializamos utter.
     var utter = new SpeechSynthesisUtterance();
     utter.rate = 1;
     utter.pitch = 0.111111;
-     utter.lang = 'es-MX';
-  
+    utter.lang = 'es-MX';
+
     // Cargamos las voces que tenemos en nuestro sistema y las mostarmos en un arreglo por consola.
     window.speechSynthesis.onvoiceschanged = function () {
       voices = window.speechSynthesis.getVoices();
       console.log(voices);
     };
-  
+
     // Definimos los comandos a utilizar.
     var commands = {
       'hola maria': function () {
@@ -37,21 +36,21 @@ startAssistant.addEventListener('click', function()
       },
       'open youtube': function () {
         utter.text = 'abriendo Youtube';
-        window.open('https://www.youtube.com', '_blank');
-  
-        // Setea la voz que queremos usar en base a nuestra lista.
         utter.voice = voices[1];
         window.speechSynthesis.speak(utter);
+        window.open('https://www.youtube.com', '_blank');
+
+        // Setea la voz que queremos usar en base a nuestra lista.
       },
       'open facebook': function () {
         utter.text = 'abriendo facebook';
-        window.open('https://www.facebook.com', '_blank');
-  
-        // Setea la voz que queremos usar en base a nuestra lista.
         utter.voice = voices[1];
         window.speechSynthesis.speak(utter);
+        window.open('https://www.facebook.com', '_blank');
+
+        // Setea la voz que queremos usar en base a nuestra lista.
       },
-      'como estas': function () {     
+      'como estas': function () {
         utter.text = 'Muy bien!';
         utter.voice = voices[1];
         window.speechSynthesis.speak(utter);
@@ -74,35 +73,29 @@ startAssistant.addEventListener('click', function()
       // Array que devuelve aleatoriamente un elemento del array, en este caso un chiste.
       'cuentame un chiste': function () {
         chistes = [
-          
           'que es lo unico blanco que tiene un negro,... el dueño',
           'que es lo unico blanco que tiene un negro,... el dueño',
           'que es lo unico blanco que tiene un negro,... el dueño',
           'que es lo unico blanco que tiene un negro,... el dueño',
           'que es lo unico blanco que tiene un negro,... el dueño',
           'que es lo unico blanco que tiene un negro,... el dueño',
-          
-          
         ];
         utter.text = chistes[Math.floor(Math.random() * chistes.length)];
         utter.voice = voices[1];
         window.speechSynthesis.speak(utter);
       },
     };
-  
+
     // Esto nos sirve para ver que escucha el programa en tiempo real.
-    
-    
-    annyang.addCallback('result', function(phrases) {
-      console.log("I think the user said: ", phrases);
-      console.log("But then again, it could be any of the following: ", phrases);
-      
-      var userSaid =  document.getElementById('userIsSaying');
-      userSaid.innerHTML = phrases[0]; 
-        
-          });
-          
-  
+
+    annyang.addCallback('result', function (phrases) {
+      console.log('I think the user said: ', phrases);
+      console.log('But then again, it could be any of the following: ', phrases);
+
+      var userSaid = document.getElementById('userIsSaying');
+      userSaid.innerHTML = phrases[0];
+    });
+
     // Sumamos todos los comandos a annyang.
     annyang.addCommands(commands);
     // Establecer el idioma
@@ -110,5 +103,4 @@ startAssistant.addEventListener('click', function()
     // Annyang comienza a escuchar.
     annyang.start();
   }
-  });
-
+});
